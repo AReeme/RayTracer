88789e2c6db323fa3405e184713d39cf71441d16
AReeme <areeme@student.neumont.edu>
2022-12-07 19:35:46 -0700

    Ray Tracer Plane Completed

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -1,12 +1,13 @@
-#include"Renderer/Renderer.h"
+#include "Renderer/Renderer.h"
 #include "Objects/Scene.h"
-#include<iostream>
+#include "Objects/Plane.h"
+#include <iostream>
 
 int main(int, char**)
 {
 	const int width = 800;
 	const int height = 600;
-	const int samples = 1;
+	const int samples = 10;
 
 	Renderer renderer; 
 	renderer.Initialize();
@@ -16,9 +17,9 @@ int main(int, char**)
 	Camera camera({ 0, 1, 2 }, { 0, 0, 0 }, { 0, 1, 0 }, 60.0f, width / (float)height);
 	Scene scene;
 
-	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ 0, 1, 0 })));
-	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 1.25f, 0, -1 }, 0.5f, std::make_unique<Metal>(color3{ 1, 1, 1 }, 0.3f)));
-	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, -100.5f, -1 }, 100.0f, std::make_unique<Lambertian>(color3{ 0.2f, 0.2f, 0.2f })));
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0.5f, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ 0, 1, 0 })));
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 1.25f, 0.5f, -1 }, 0.5f, std::make_unique<Metal>(color3{ 1, 1, 1 }, 0.3f)));
+	scene.AddObject(std::make_unique<Plane>(glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, std::make_unique<Lambertian>(color3{ 0.2f, 0.2f, 0.2f })));
 
 	bool quit = false;
 	while (!quit)
